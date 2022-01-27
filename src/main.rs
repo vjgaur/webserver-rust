d327c23142924c0dab5f8ce05ae81612190297e0
vjgaur <vijayendra.gaur@gmail.com>
2022-01-27 13:31:05 +0530

    code refactoring

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,24 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer: [u8; 1024] = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    let contents = std::fs::read_to_string("index.html").unwrap();
+    let get: &[u8; 16] = b"GET / HTTP/1.1\r\n";
+    let sleep: &[u8; 21] = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK,", "index.html")
+    } else {
+        ("HTTP/1.1 404 Not Found,", "404.html")
+    };
+    let contents: String = std::fs::read_to_string(filename).unwrap();
     let response: String = format!(
-        "HTTP/1.1 200 OK\r\n Content-Lenght: {}\r\n\r\n{}",
+        "{}\r\n Content-Lenght: {}\r\n\r\n{}",
+        status_line,
         contents.len(),
         contents
     );
+
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+
     //    println!("Request: {}", String::from_utf8_lossy(&buffer[..]))
 }
